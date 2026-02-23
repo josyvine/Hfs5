f463cb2986ba01f5f8dcef84456b19779e331b0d
josyvine <64621103+josyvine@users.noreply.github.com>
2026-02-24 01:51:48 +0530

    Update AirplaneModeReceiver.java

--- app/src/main/java/com/app/hfs/security/receivers/AirplaneModeReceiver.java
+++ app/src/main/java/com/app/hfs/security/receivers/AirplaneModeReceiver.java
@@ -9,7 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.provider.Settings;
-import android.util.Log;
+import android.util.Log; 
 
 import androidx.core.app.NotificationCompat;
 
